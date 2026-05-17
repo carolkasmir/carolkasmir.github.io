e1ffc6057d8c706718a7f50020fe0effc1f37913
Carolyne Aming'a <machokacarolyne@gmail.com>
2026-05-17 13:50:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const slides = document.querySelectorAll('.slide');
 
-function revealOnScroll() {
+function revealOnScroll(){
+
   slides.forEach(slide => {
+
     const slideTop = slide.getBoundingClientRect().top;
+
     const windowHeight = window.innerHeight;
 
-    if (slideTop < windowHeight - 100) {
+    if(slideTop < windowHeight - 100){
+
       slide.classList.add('show');
+
     }
+
   });
+
 }
 
 window.addEventListener('scroll', revealOnScroll);
+
 window.addEventListener('load', revealOnScroll);
